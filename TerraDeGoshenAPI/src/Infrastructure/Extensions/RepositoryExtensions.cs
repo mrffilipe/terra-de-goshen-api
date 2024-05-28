dbd4c4a9d06d3c83be25ec97502b6ea494e6bbb8
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-05-28 19:12:27 -0300

    Add cleanup to the RepositoryExtensions class

--- TerraDeGoshenAPI/src/Infrastructure/Extensions/RepositoryExtensions.cs
+++ TerraDeGoshenAPI/src/Infrastructure/Extensions/RepositoryExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Google.Cloud.Storage.V1;
-using TerraDeGoshenAPI.src.Domain;
+﻿using TerraDeGoshenAPI.src.Domain;
 
 namespace TerraDeGoshenAPI.src.Infrastructure
 {
@@ -7,13 +6,6 @@ namespace TerraDeGoshenAPI.src.Infrastructure
     {
         public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
         {
-            //services.AddScoped<IImageRepository>(provider =>
-            //{
-            //    var storageClient = provider.GetRequiredService<StorageClient>();
-            //    var bucketName = configuration["mrffilipe_bucket"];
-            //    return new ImageRepository(storageClient, bucketName);
-            //});
-
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IImageRepository, ImageRepository>();
 
